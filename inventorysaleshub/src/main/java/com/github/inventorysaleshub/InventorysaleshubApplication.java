968fbcfc0e8dc94a4fa819d4e7a5e8630ea36747
Sergio E. Ramos <virtualsoft.adm@gmail.com>
2025-05-29 23:33:39 +0200

    Creation Spring Boot Proyect

---
+++ inventorysaleshub/src/main/java/com/github/inventorysaleshub/InventorysaleshubApplication.java
@@ -0,0 +1,13 @@
+package com.github.inventorysaleshub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventorysaleshubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventorysaleshubApplication.class, args);
+	}
+
+}
---
+++ inventorysaleshub/src/test/java/com/github/inventorysaleshub/InventorysaleshubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.inventorysaleshub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventorysaleshubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
