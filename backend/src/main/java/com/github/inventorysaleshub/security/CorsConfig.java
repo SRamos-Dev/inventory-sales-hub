6be91db2bad1d6cfc0323b8e0e7e6ded2f8586e9
Sergio E. Ramos <virtualsoft.adm@gmail.com>
2025-12-28 14:59:25 +0100

    Update CorsConfig.java

--- backend/src/main/java/com/github/inventorysaleshub/security/CorsConfig.java
+++ backend/src/main/java/com/github/inventorysaleshub/security/CorsConfig.java
@@ -1,4 +1,4 @@
-package com.github.inventorysaleshub.config;
+package com.github.inventorysaleshub.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
